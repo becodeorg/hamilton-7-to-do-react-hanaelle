cf96c766a9a1c9a8a4930416c60713eb1318d277
hanaelle <hananeelkarouni1@gmail.com>
2022-11-22 17:02:52 +0100

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 //import "./App.css";
+import React, { useState, useEffect } from "react"; //useEffect enregistre nos taches dans localStorage ==> 
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 import Form from "./components/Form";
-import React, { useState } from "react";
 
+//const LSKEY = "MyTodoApp";
 
 function App() {
   const initialTodos = [
     { texte: "Learn react", check: false },
     { texte: "Be awesome", check: false },
   ]; //Ds le tableau on peut rajouter des elements
+
+  useEffect(() => {
+    //(useEffect) permet de dire a react qu a chause mise a jour  de notre composent Appjs copie nos taches et enregistre le dans localStorage
+    window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
+  } /*[todos] */ ); //on applique notre changelent dans le tableau  ex [todos]
 
   const [todos, setTodos] = useState(initialTodos);
   return (
